1e8c8233d90d4a6cd9499208fcc7d9aa27fd9d5f
Nobuhiro Ito <iseebi@iseteki.net>
2017-01-11 20:16:01 +0900

    process file download

---
+++ TransporterPad/StringExtensions.swift
@@ -0,0 +1,57 @@
+//
+//  StringExtensions.swift
+//  TransporterPad
+//
+//  Created by Nobuhiro Ito on 2017/01/11.
+//  Copyright © 2017年 Nobuhiro Ito. All rights reserved.
+//
+
+import Foundation
+
+// http://qiita.com/su_k/items/77345499e04de7f214ad
+extension String {
+    
+    private var ns: NSString {
+        return (self as NSString)
+    }
+    
+    public func substring(from index: Int) -> String {
+        return ns.substring(from: index)
+    }
+    
+    public func substring(to index: Int) -> String {
+        return ns.substring(to: index)
+    }
+    
+    public func substring(with range: NSRange) -> String {
+        return ns.substring(with: range)
+    }
+    
+    public var lastPathComponent: String {
+        return ns.lastPathComponent
+    }
+    
+    public var pathExtension: String {
+        return ns.pathExtension
+    }
+    
+    public var deletingLastPathComponent: String {
+        return ns.deletingLastPathComponent
+    }
+    
+    public var deletingPathExtension: String {
+        return ns.deletingPathExtension
+    }
+    
+    public var pathComponents: [String] {
+        return ns.pathComponents
+    }
+    
+    public func appendingPathComponent(_ str: String) -> String {
+        return ns.appendingPathComponent(str)
+    }
+    
+    public func appendingPathExtension(_ str: String) -> String? {
+        return ns.appendingPathExtension(str)
+    }
+}
--- TransporterPad/ViewController.swift
+++ TransporterPad/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import CoreFoundation
+import Alamofire
 
 class ViewController: NSViewController {
 
@@ -25,10 +27,66 @@ extension ViewController : DragTargetViewDelegate {
 
     func dragTargetView(_ dragTargetView: DragTargetView, dropRemoteURL fileName: String) {
         NSLog("Remote file:%@", fileName)
+        
+        downloadFile(remoteURLString: fileName)
     }
     
     func dragTargetView(_ dragTargetView: DragTargetView, dropLocalFilePath fileName: String) {
         NSLog("Local file:%@", fileName)
+        
+        processBinary(fileName: fileName)
+    }
+    
+    func processBinary(fileName: String) {
+        // ipa かどうか
+        
+        // apk かどうか
+    }
+
+    func downloadFile(remoteURLString: String) {
+        guard let cacheDir = cacheDir() else { return }
+        let cachePath = cacheFilePath(remoteURLString: remoteURLString, cacheDir: cacheDir)
+        
+        let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+            return (cachePath, [.removePreviousFile, .createIntermediateDirectories])
+        }
+        
+        Alamofire.download(remoteURLString, to: destination).response { [weak self] response in
+            if response.error == nil, let resultPath = response.destinationURL?.path {
+                self?.processBinary(fileName: resultPath)
+            }
+        }
+    }
+    
+    func cacheFilePath(remoteURLString: String, cacheDir: URL) -> URL {
+        var fileURL: URL = cacheDir
+        repeat {
+            let ext = remoteURLString.pathExtension
+            let fileName = randomStr(count: 16).appending(ext)
+            fileURL = cacheDir.appendingPathComponent(fileName)
+        } while (!FileManager.default.fileExists(atPath: fileURL.absoluteString))
+        return fileURL
+    }
+    
+    func randomStr(count: Int) -> String {
+        let chars = "abcdefghijklnmopqrstuvwxyz0123456789"
+        var rv = ""
+        for _ in 0..<count {
+            let startIndex = Int(arc4random_uniform(UInt32(chars.lengthOfBytes(using: .utf8))))
+            rv.append(chars.substring(with:
+                chars.index(chars.startIndex, offsetBy: startIndex)..<chars.index(chars.startIndex, offsetBy: startIndex + 1)
+                ))
+        }
+        return rv
+    }
+
+    func cacheDir() -> URL? {
+        guard let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
+        let binDir = cacheDir.appendingPathComponent("binaries", isDirectory: true)
+        if (!FileManager.default.fileExists(atPath: binDir.absoluteString)) {
+            if (try? FileManager.default.createDirectory(at: binDir, withIntermediateDirectories: true, attributes: nil)) == nil { return nil }
+        }
+        return binDir
     }
     
 }
