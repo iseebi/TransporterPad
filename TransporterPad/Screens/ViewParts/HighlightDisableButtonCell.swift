cb3bc03ad0ac9daa728c5d0ef23d82618128d8d9
Nobuhiro Ito <iseebi@iseteki.net>
2017-10-07 12:25:16 +0900

    highlight disable for images

---
+++ TransporterPad/Screens/ViewParts/HighlightDisableButtonCell.swift
@@ -0,0 +1,22 @@
+//
+//  HighlightDisableButtonCell.swift
+//  TransporterPad
+//
+//  Created by Nobuhiro Ito on 10/7/17.
+//  Copyright © 2017 Nobuhiro Ito. All rights reserved.
+//
+//  This file is part of TransporterPad. Licensed in GPLv3.
+//
+
+import Cocoa
+
+class HighlightDisableButtonCell: NSButtonCell {
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func highlight(_ flag: Bool, withFrame cellFrame: NSRect, in controlView: NSView) {
+        
+    }
+}
